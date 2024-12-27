6e25ab176bf469ae2bded95191844cc7e038f058
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2024-12-27 04:14:16 +0300

    fix: update import path for AddMaterialForm in MaterialList component

--- src/components/ui/material-components/materialList.tsx
+++ src/components/ui/material-components/materialList.tsx
@@ -9,7 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "@/utilities/redux/store";
 import { fetchMaterials } from "@/utilities/redux/slices/materialSlice";
 import { Badge, Button, Input, Modal, Space, Table, TableColumnsType } from "antd";
-import AddMaterialForm from "./addMaterialForm";
+import AddMaterialForm from "@/components/ui/material-components/addMaterialForm";
 
 export default function MaterialList() {
   const dispatch: AppDispatch = useDispatch();
