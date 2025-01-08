dea5b179a159f9d9171a4b6ad5d98d734c667d5f
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-01-08 18:26:29 +0300

    Refactor customer add and update forms for improved UI and usability

--- src/components/ui/customer-components/addCustomerForm.tsx
+++ src/components/ui/customer-components/addCustomerForm.tsx
@@ -5,7 +5,11 @@ import { AppDispatch } from "@/utilities/redux/store";
 import { addCustomer } from "@/utilities/redux/slices/customerSlice";
 import { Button, Form, Input } from "antd";
 
-export default function CustomerAddForm({ onSuccess }: { onSuccess: () => void }) {
+export default function CustomerAddForm({
+  onSuccess,
+}: {
+  onSuccess: () => void;
+}) {
   const dispatch = useDispatch<AppDispatch>();
   const [form] = Form.useForm();
 
@@ -21,49 +25,101 @@ export default function CustomerAddForm({ onSuccess }: { onSuccess: () => void }
 
   return (
     <>
-      <h2 className="text-2xl font-bold text-center mb-6">Yeni Müşteri Ekle</h2>
-      <Form name="addCustomerForm" form={form} onFinish={onFinish} layout="vertical" className="flex flex-col justify-between">
-        <div className="container flex flex-col px-6 md:mx-auto md:px-12 md:flex-row md:justify-center md:gap-5">
-          <div className="w-full md:w-3/5">
-            <Form.Item name="companyName" label="Şirket Adı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="contactName" label="Yetkili Adı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="contactTitle" label="Yetkili Ünvanı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-          </div>
-          <div className="w-full md:w-3/5">
-            <Form.Item name="email" label="E-posta" rules={[{ required: true, type: "email", message: "Geçerli bir e-posta girin" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="phone" label="Telefon" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="taxNumber" label="Vergi Numarası" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-          </div>
-
-          <div className="w-full md:w-3/5">
-            <Form.Item name="postalCode" label="Posta Kodu" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-
-            <Form.Item name="city" label="Şehir" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-            <Form.Item name="country" label="Ülke" rules={[{ required: true, message: "Zorunlu alan" }]}>
-              <Input />
-            </Form.Item>
-          </div>
-
-          <Form.Item name="address" label="Adres" rules={[{ required: true, message: "Zorunlu alan" }]}>
-            <Input.TextArea />
+      <h2 className="text-2xl font-bold text-center mb-8">Yeni Müşteri Ekle</h2>
+      <Form
+        name="addCustomerForm"
+        form={form}
+        onFinish={onFinish}
+        layout="vertical"
+        className="mx-auto p-8 bg-white rounded-lg"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
+          <Form.Item
+            name="companyName"
+            label="Şirket Adı"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
           </Form.Item>
-          <Button type="primary" htmlType="submit">
+          <Form.Item
+            name="contactName"
+            label="Yetkili Adı"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="contactTitle"
+            label="Yetkili Ünvanı"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="email"
+            label="E-posta"
+            rules={[
+              {
+                required: true,
+                type: "email",
+                message: "Geçerli bir e-posta girin",
+              },
+            ]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="phone"
+            label="Telefon"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="taxNumber"
+            label="Vergi Numarası"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="postalCode"
+            label="Posta Kodu"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="city"
+            label="Şehir"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+          <Form.Item
+            name="country"
+            label="Ülke"
+            rules={[{ required: true, message: "Zorunlu alan" }]}
+          >
+            <Input className="h-10" />
+          </Form.Item>
+        </div>
+
+        <Form.Item
+          name="address"
+          label="Adres"
+          className="mt-6"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input.TextArea rows={4} />
+        </Form.Item>
+
+        <div className="flex justify-end mt-8">
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="px-8 h-10 text-white bg-blue-600 hover:bg-blue-700 focus:ring-2 focus:ring-blue-500"
+          >
             Ekle
           </Button>
         </div>
--- src/components/ui/customer-components/updateCustomerForm.tsx
+++ src/components/ui/customer-components/updateCustomerForm.tsx
@@ -37,35 +37,82 @@ export default function UpdateCustomerForm({
   }, [initialValues, form]);
 
   return (
-    <Form form={form} initialValues={initialValues || {}} onFinish={onFinish} layout="horizontal">
-      <Form.Item name="companyName" label="Şirket Adı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="contactName" label="Yetkili Adı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="contactTitle" label="Yetkili Ünvanı" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="email" label="E-posta" rules={[{ required: true, type: "email", message: "Geçerli bir e-posta girin" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="phone" label="Telefon" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="taxNumber" label="Vergi Numarası">
-        <Input />
-      </Form.Item>
-      <Form.Item name="postalCode" label="Posta Kodu">
-        <Input />
-      </Form.Item>
-      <Form.Item name="city" label="Şehir" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="country" label="Ülke" rules={[{ required: true, message: "Zorunlu alan" }]}>
-        <Input />
-      </Form.Item>
-      <Form.Item name="address" label="Adres" rules={[{ required: true, message: "Zorunlu alan" }]}>
+    <Form
+      form={form}
+      initialValues={initialValues || {}}
+      onFinish={onFinish}
+      layout="vertical"
+      className="mx-auto p-8 bg-white rounded-lg"
+    >
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-1">
+        <Form.Item
+          name="companyName"
+          label="Şirket Adı"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="contactName"
+          label="Yetkili Adı"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="contactTitle"
+          label="Yetkili Ünvanı"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-posta"
+          rules={[
+            {
+              required: true,
+              type: "email",
+              message: "Geçerli bir e-posta girin",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Telefon"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="taxNumber" label="Vergi Numarası">
+          <Input />
+        </Form.Item>
+        <Form.Item name="postalCode" label="Posta Kodu">
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="city"
+          label="Şehir"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="country"
+          label="Ülke"
+          rules={[{ required: true, message: "Zorunlu alan" }]}
+        >
+          <Input />
+        </Form.Item>
+      </div>
+
+      <Form.Item
+        name="address"
+        label="Adres"
+        rules={[{ required: true, message: "Zorunlu alan" }]}
+      >
         <Input.TextArea />
       </Form.Item>
 
