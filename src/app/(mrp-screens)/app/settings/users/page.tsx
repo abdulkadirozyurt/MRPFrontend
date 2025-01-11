fee97e21730e354ad51c725676cf1c7621174c4b
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-01-11 03:50:39 +0300

    .

--- src/app/(mrp-screens)/app/settings/users/page.tsx
+++ src/app/(mrp-screens)/app/settings/users/page.tsx
@@ -1,9 +1,8 @@
 "use client"
 
-import SupplierList from "@/components/ui/supplier-components/supplierList";
 import ProtectedRoute from "@/components/ProtectedRoute";
-import { UserRoles } from "@/utilities/constants/UserRoles";
 import UserList from "@/components/ui/user-management-components/userList";
+import { UserRoles } from "@/utilities/constants/UserRoles";
 
 export default function Users() {
   return (
--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,4 +1,43 @@
-"use client"
+// "use client"
+
+// import { ReactNode, useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import { RootState } from "@/utilities/redux/store";
+// import { LoadingOutlined } from "@ant-design/icons";
+// import { useRouter } from "next/navigation";
+// import { UserRoles } from "@/utilities/constants/UserRoles";
+
+// interface ProtectedRouteProps {
+//   children: ReactNode;
+//   allowedRoles?: string[];
+// }
+
+// export default function ProtectedRoute({ children, allowedRoles }: ProtectedRouteProps) {
+//   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
+//   const router = useRouter();
+//   const userRole = useSelector((state: RootState) => state.auth.userRole);
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     if (!isAuthenticated) {
+//       router.push("/login");
+//       setLoading(false);
+//     } else if (!allowedRoles?.includes(userRole as UserRoles)) {
+//       router.push("/unauthorized");
+//       setLoading(false);
+//     } else {
+//       setLoading(false);
+//     }
+//   }, [isAuthenticated, router, userRole, allowedRoles]);
+
+//   if (loading) {
+//     return <LoadingOutlined />;
+//   }
+
+//   return <>{children}</>;
+// }
+
+"use client";
 
 import { ReactNode, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
@@ -12,7 +51,7 @@ interface ProtectedRouteProps {
   allowedRoles?: string[];
 }
 
-export default function ProtectedRoute({ children, allowedRoles }: ProtectedRouteProps) {
+export default function ProtectedRoute({ children, allowedRoles = [] }: ProtectedRouteProps) {
   const isAuthenticated = useSelector((state: RootState) => state.auth.isAuthenticated);
   const router = useRouter();
   const userRole = useSelector((state: RootState) => state.auth.userRole);
@@ -21,17 +60,14 @@ export default function ProtectedRoute({ children, allowedRoles }: ProtectedRout
   useEffect(() => {
     if (!isAuthenticated) {
       router.push("/login");
-      setLoading(false);
-    } else if (!allowedRoles?.includes(userRole as UserRoles)) {
+    } else if (!allowedRoles.includes(userRole as UserRoles)) {
       router.push("/unauthorized");
-      setLoading(false);
-    } else {
-      setLoading(false);
     }
-  }, [isAuthenticated, router, userRole, allowedRoles]);
+    setLoading(false);
+  }, [isAuthenticated, router, userRole]);
 
   if (loading) {
-    return <LoadingOutlined />;
+    return <div style={{ textAlign: "center", marginTop: "20px" }}>Yükleniyor...</div>;
   }
 
   return <>{children}</>;
