360fd1a440617b88fe6fad70163f1134cdaba86e
abdulkadirozyurt <abdulkadir.ozyurt@gmail.com>
2025-01-11 03:45:00 +0300

    Add "use client" directive in user settings page and remove debug log from navbar

--- src/app/(mrp-screens)/app/settings/users/page.tsx
+++ src/app/(mrp-screens)/app/settings/users/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import SupplierList from "@/components/ui/supplier-components/supplierList";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import { UserRoles } from "@/utilities/constants/UserRoles";
--- src/components/ui/mrp-app-layout-components/navbar.tsx
+++ src/components/ui/mrp-app-layout-components/navbar.tsx
@@ -18,7 +18,6 @@ export default function Navbar() {
   const router = useRouter();
   const dispatch: AppDispatch = useDispatch();
   const userRole = useSelector((state: RootState) => state.auth.userRole);
-  console.log("Navbar userRole:", userRole);
 
   const handleLogout = () => {
     dispatch(logout());
